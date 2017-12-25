da7451c38eb7ffdac3bb411f7581b8a1d7195adf
Adam Clarke <9417231+Adam-Clrk@users.noreply.github.com>
2017-12-25 12:53:57 +0000

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+/*$('section>div').click(function (e) {
+  var parent = $(e.currentTarget).parent();
+  console.log(parent);
+  if (parent.hasClass('modal')) {
+    parent.addClass('closemodal')
+    parent.removeClass('modal')
+  } else {
+    parent.addClass('modal')
+    parent.removeClass('closemodal')
+  }
+})
+*/
